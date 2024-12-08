0894213765d253eb3bb0e668464a86328ea6e3fa
Yoon <leeon592@gmail.com>
2024-12-08 00:15:59 -0500

    Update APIHandler.tsx

--- frontend/utils/APIHandler.tsx
+++ frontend/utils/APIHandler.tsx
@@ -1,31 +1,21 @@
+import * as AuthSession from 'expo-auth-session';
+
 export class APIHandler {
   static backendUrl = 'http://localhost:8000';
 
-  static async getOAuthUrl(): Promise<string> {
-    try {
-      const response = await fetch(`${this.backendUrl}/api/auth/login`);
-      if (!response.ok) throw new Error('Failed to fetch OAuth URL');
-      return response.url;
-    } catch (error) {
-      console.error('Error fetching OAuth URL:', error);
-      throw error;
-    }
-  }
-
   static async initiateLogin(platform: 'mobile' | 'web', setShowWebView: Function, setAuthUrl: Function) {
-    try {
-      const authUrl = await this.getOAuthUrl();
+    const loginUrl = `${this.backendUrl}/api/auth/login?platform=${platform}`;
 
+    try {
       if (platform === 'web') {
-        // setShowWebView(true);
-        console.log('Redirecting to OAuth in browser:', authUrl);
-        window.location.href = authUrl; // Desktop browser redirect
-      } else if (platform === 'mobile') {
-        console.log('Opening OAuth in WebView:', authUrl);
-        setAuthUrl(authUrl);
-        setShowWebView(true); // Mobile WebView
+        console.log("CUrrnet web")
+        // For web
+        window.location.href = loginUrl;
       } else {
-        throw new Error('Unsupported platform');
+        console.log("CUrrnet mobile")
+        setAuthUrl(loginUrl);
+        setShowWebView(true); 
+
       }
     } catch (error) {
       console.error('Error initiating login:', error);
