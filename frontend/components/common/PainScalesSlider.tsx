7481df5d09a452f66df6a6ba7929c27339b38054
Yoon <leeon592@gmail.com>
2025-01-07 21:31:18 -0500

    update api url

--- frontend/components/common/PainScalesSlider.tsx
+++ frontend/components/common/PainScalesSlider.tsx
@@ -30,7 +30,7 @@ const PainScaleSlider: React.FC<PainScaleSliderProps> = ({
 
   const handleSubmitPainSeverity = async () => {
     try {
-      await makeRequest("http://localhost:8000/api/dex/store_user_input", {
+      await makeRequest("https://vugle-backend-v1.com/api/dex/store_user_input", {
         userInput: painScale,
         inputType: "painSeverity",
       });
